86fcd5cd5f3bf7f7cbb72d8be5049e8a5587e2d3
hassankh0 <hassan.h.khalife@outlook.com>
2026-01-30 15:04:52 +0100

    correction navbar mobile

--- assets/js/load-components.js
+++ assets/js/load-components.js
@@ -7,6 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(response => response.text())
             .then(data => {
                 headerPlaceholder.outerHTML = data;
+                
+                // Reinitialize menu after header loads
+                // Use a small delay to ensure jQuery and DOM are ready
+                setTimeout(function() {
+                    if (typeof $ !== 'undefined') {
+                        mainMenu();
+                        offCanvas();
+                    }
+                }, 50);
             })
             .catch(error => console.error('Error loading header:', error));
     }
@@ -38,3 +47,57 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => console.error('Error loading footer:', error));
     }
 });
+
+// Replicate mainMenu function from common.js
+function mainMenu() {
+    var var_window = $(window),
+        navContainer = $('.header-navigation'),
+        navbarToggler = $('.navbar-toggler'),
+        navMenu = $('.theme-nav-menu'),
+        navMenuLi = $('.theme-nav-menu ul li ul li'),
+        closeIcon = $('.navbar-close');
+    
+    navbarToggler.on('click', function() {
+        navbarToggler.toggleClass('active');
+        navMenu.toggleClass('menu-on');
+    });
+    
+    closeIcon.on('click', function() {
+        navMenu.removeClass('menu-on');
+        navbarToggler.removeClass('active');
+    });
+    
+    navMenu.find("li a").each(function() {
+        if ($(this).children('.dd-trigger').length < 1) {
+            if ($(this).next().length > 0) {
+                $(this).append('<span class="dd-trigger"><i class="far fa-angle-down"></i></span>')
+            }
+        }
+    });
+    
+    navMenu.on('click', '.dd-trigger', function(e) {
+        e.preventDefault();
+        $(this).parent().parent().siblings().children('ul.sub-menu').slideUp();
+        $(this).parent().next('ul.sub-menu').stop(true, true).slideToggle(350);
+        $(this).toggleClass('sub-menu-open');
+    });
+}
+
+// Replicate offCanvas function from common.js
+function offCanvas() {
+    const $overlay = $(".offcanvas__overlay");
+    const $toggler = $(".navbar-toggler");
+    const $menu = $(".theme-nav-menu");
+    
+    $toggler.add($overlay).add(".navbar-close, .panel-close-btn").on("click", function() {
+        $overlay.toggleClass("overlay-open");
+        if ($(this).is($overlay)) {
+            $toggler.removeClass("active");
+            $menu.removeClass("menu-on");
+        }
+    });
+    
+    $(window).on("resize", function() {
+        if ($(window).width() > 991) $overlay.removeClass("overlay-open");
+    });
+}
